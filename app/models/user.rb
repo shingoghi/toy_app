30ed9060dbdfd16c75bfc0a7962e1215fd42aebc
shingo <ggg@globalhotline.jp>
2017-08-02 19:00:58 +0900

    Finish toy app

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
     has_many :microposts
-
-    def hoge
-        print("hogehoge!!")
-    end
+# :userというデータベースのカラム名（列名）は存在しない。
+# db/schema.rbを見てみる
+# validates 対象は存在するカラムしか指定できない
+validates :name, presence: true    # 「FILL_IN」をコードに置き換えてください
+# これは validates(:name, presence: true)
+validates :email, presence: true    # 「FILL_IN」をコードに置き換えてください
 end
